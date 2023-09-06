3ef5fdf46636a66022638890d5606e9f57f0af6e
Hank-Gurung <gurungnishank@gmail.com>
2023-09-06 22:27:50 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+// Rock Paper Scissor game
+
+const playerText = document.querySelector("#playerText")
+const computerText = document.querySelector("#computerText")
+const resultText = document.querySelector("#resultText")
+const choiceBtns = document.querySelectorAll(".choiceBtn")
+let player;
+let computer;
+let result;
+
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`
+    computerText.textContent = `Computer: ${computer}`
+    resultText.textContent = checkWinner()
+
+}))
+
+function computerTurn(){
+    const randNum = Math.floor(Math.random()*3)+1
+
+    switch(randNum){
+        case 1:
+            computer = "✊"
+            break
+        case 2:
+            computer = "✋"
+            break
+        case 3:
+            computer = "✌️"
+            break
+    }
+}
+function checkWinner(){
+    if(player == computer){
+        return "Draw"
+    }
+    else if(computer == "✊"){
+        return (player == "✋") ? "You Win!" : "You Lose!"
+    }
+    else if(computer == "✋"){
+        return (player == "✌️") ? "You Win!" : "You Lose!"
+    }
+    else if(computer == "✌️"){
+        return (player == "✊") ? "You Win!" : "You Lose!"
+    }
+}
+
+// Rock Paper Scissor game
+
+//data selection method
+// const choiceBtns = document.querySelectorAll('[data-selection]')
+// choiceBtns.forEach(button => {button.addEventListener('click', () => {
+//     player = button.dataset.selection;
+//     console.log(player)
+//     computerTurn();
+//     playerText.textContent = `Player: ${player}`
+//     computerText.textContent = `Computer: {computer}`
+//     resultText.textContent = checkWinner()
+// })
+// })
